552d4dfa25e9a616905523d6f0be5462001f07dc
IvanTurbin <ivan.turbin@gmail.com>
2021-04-01 12:53:27 +0300

    Iterator

--- src/Stack.java
+++ src/Stack.java
@@ -1,7 +1,8 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 
-public class Stack {
+public class Stack implements Iterable {
     private ArrayList<String> stack;
     private int index = -1;
 
@@ -28,6 +29,26 @@ public class Stack {
         System.out.println(stack);
     }
 
+
+    @Override
+    public Iterator iterator(){
+        return new Iterator<>(){
+
+            private int currentIndex = -1;
+
+            @Override
+            public boolean hasNext() {
+                return currentIndex < stack.size() - 1;
+            }
+
+            @Override
+            public String next() {
+                currentIndex++;
+                return stack.get(currentIndex);
+            }
+        };
+    }
+
     public static void main(String[] args) {
         Stack stack = new Stack();
         stack.printStack();
@@ -37,6 +58,13 @@ public class Stack {
         stack.printStack();
         stack.push("tiistai");
         stack.printStack();
+
+
+        Iterator stackIterator = stack.iterator();
+
+        while (stackIterator.hasNext()) {
+            System.out.println(stackIterator.next());
+        }
 
         stack.pop();
         stack.printStack();
